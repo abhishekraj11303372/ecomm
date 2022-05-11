591e8dd26b8707110cafac0174eae79d0f141b9f
Abhishek Raj <abhishekraj1130@gmail.com>
2022-05-11 19:36:47 +0530

    asynchronous loading from bootstrap

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,6 +1,1 @@
-import faker from 'faker';
-
-let cartText = `<div>You have ${faker.random.number()} items in your cart</div>`;
-
-// console.log(products);
-document.querySelector('.dev-cart').innerHTML = cartText;
+import('./bootstrap')
